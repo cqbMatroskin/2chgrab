fd37129572ca455c009f3c4451f11e053ad0982e
cqbMatroskin <cqb.matroskin@gmail.com>
2019-06-03 12:33:01 +0400

    fix

--- parser.py
+++ parser.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
-import requests
 
 import constants
 
 
 def get_links(html, limit=None):
     soup = BeautifulSoup(html, "lxml")
-    content = soup.findAll('a', attrs='post__image-link', limit=limit)
+    content = soup.findAll('a', attrs='post__image-link')
     links = []
     for post in content:
-        links.append(constants.BASE_URL + post.get('href'))
+        if len(links) < limit:
+            if not ('mp4' or 'webm') in post.get('href'):
+                links.append(constants.BASE_URL + post.get('href'))
     return links
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import requests
 import os
 import uuid
+import re
 import time
 
 import constants
 import parser
+
 
 # def enter_data():
 #
@@ -30,20 +32,23 @@ def parse_page(html, limit=None):
 
 
 def create_directory():
-    dir_name = input('Enter directory name ')
-    dir_name.strip().replace(' ', '_')
-    if not os.path.exists(constants.DOWNLOAD_PATH.format(dir_name)):
-        os.mkdir(constants.DOWNLOAD_PATH.format(dir_name))
-        name = constants.DOWNLOAD_PATH.format(dir_name)
+    dir_name = constants.DOWNLOAD_PATH.format(time.strftime('%Y-%m-%d'))
+    if not os.path.exists(dir_name):
+        os.mkdir(dir_name)
         print('Directory {} is created'.format(dir_name))
-        return name
+    return dir_name
 
 
 def dl_image(links, sleep=1):
     path = create_directory()
+    pattern = re.compile('(jpg|jpeg|png)')
     print('Download {} images ...'.format(len(links)))
     for link in links:
         image = requests.get(link)
-        with open(path + '/{0}.jpg'.format(uuid.uuid4().hex), 'wb') as temp_file:
+        image_type = image.headers['Content-Type'].replace('image/', '')
+        results = re.match(pattern, image_type).string
+        print(results)
+        with open(path + '/{0}.{1}'.format(uuid.uuid4().hex, image_type), 'wb') as temp_file:
             temp_file.write(image.content)
+
         time.sleep(sleep)
